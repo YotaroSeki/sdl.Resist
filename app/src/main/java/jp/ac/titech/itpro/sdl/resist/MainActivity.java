33d0def28819a6baf4439247d7763e450de6f5af
YotaroSeki <youyoutarou.dog0115@gmail.com>
2019-05-17 16:55:31 +0900

    もちろん俺らは抵抗するで 拳で👊😎

--- app/src/main/java/jp/ac/titech/itpro/sdl/resist/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/resist/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private RotationView rotationView;
     private SensorManager manager;
     private Sensor gyroscope;
+    private float rotation;
+    private long previous = 0;
+    private long timeStamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +55,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        timeStamp = event.timestamp;
         float omegaZ = event.values[2];  // z-axis angular velocity (rad/sec)
-        // TODO: calculate right direction that cancels the rotation
-        rotationView.setDirection(omegaZ);
+        long dt = (timeStamp - previous);
+        rotation += (omegaZ * dt) / 1000000000;
+        rotationView.setDirection(rotation);
+        previous = timeStamp;
     }
 
     @Override
